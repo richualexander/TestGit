a4aace2c45306e7495715756730f4f9e5e54334f
richualexander <richujalex@gmail.com>
2017-05-09 16:43:30 +0530

    Test2

---
+++ richu.java
@@ -0,0 +1,13 @@
+package com.example.richu.testgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class richu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_richu);
+    }
+}
